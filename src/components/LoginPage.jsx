b8015d88b615cad48dc71654c5fc2f7af3c2fc5f
Vladimir Drannikov <krowen@inbox.ru>
2021-08-10 10:31:33 +0600

    Added validation to loginPage

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useFormik } from 'formik';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import cn from 'classnames';
 import useUser from '../hooks/index.jsx';
 import routes from '../routes.js';
 
 const LoginPage = () => {
   const user = useUser();
+  const inputRef = useRef();
   const history = useHistory();
   const formik = useFormik({
     initialValues: {
@@ -15,16 +17,34 @@ const LoginPage = () => {
       password: '',
     },
     onSubmit: async (values, actions) => {
+      actions.setStatus(false);
       try {
         const { data } = await axios.post(routes.loginPath(), values);
         user.logIn(data);
         history.push('/');
         actions.resetForm();
       } catch (err) {
-        console.log(err);
+        if (err.response.status === 401) {
+          actions.setStatus(true);
+          inputRef.current.select();
+          return;
+        }
+        throw err;
       }
     },
   });
+
+  const classFeedback = cn('form-login__feedback', {
+    'form-login__feedback--show': formik.status,
+  });
+
+  const classInput = cn('form-login__input', {
+    'form-login__input--error': formik.status,
+  });
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="form-login__wrapper">
@@ -33,26 +53,36 @@ const LoginPage = () => {
         <div className="form-login__group">
           <label className="form-login__label" htmlFor="name">Имя пользователя</label>
           <input
-            className="form-login__input"
+            className={classInput}
             id="name"
             name="username"
             type="text"
             onChange={formik.handleChange}
             value={formik.values.username}
+            ref={inputRef}
+            disabled={formik.isSubmitting}
           />
         </div>
         <div className="form-login__group">
           <label className="form-login__label" htmlFor="password">Пароль</label>
           <input
-            className="form-login__input"
+            className={classInput}
             id="password"
             name="password"
             type="password"
             onChange={formik.handleChange}
             value={formik.values.password}
+            disabled={formik.isSubmitting}
           />
         </div>
-        <button className="form-login__button" type="submit">Войти</button>
+        <div className={classFeedback}>Неправильные имя пользователя или пароль</div>
+        <button
+          className="form-login__button"
+          type="submit"
+          disabled={formik.isSubmitting}
+        >
+          Войти
+        </button>
       </form>
     </div>
   );
