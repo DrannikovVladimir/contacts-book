600a23b2287f048b9c237cf96231e63b7dc33db2
Vladimir Drannikov <krowen@inbox.ru>
2021-08-12 14:48:33 +0600

    Added feedback to listContacts

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -4,6 +4,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { modalOpen } from '../slices/modalSlice.js';
 import { contactsSelector } from '../slices/selectors.js';
 
+const getFeedback = (status) => {
+  switch (status) {
+    case 'default':
+      return <p className="contacts-list__feedback">Список контактов пуст</p>;
+    case 'filtered':
+      return <p className="contacts-list__feedback">Ничего не найдено</p>;
+    default:
+      throw new Error(`Unknown status ${status}`);
+  }
+};
+
 const ContactsList = () => {
   const dispatch = useDispatch();
   const { contacts, filteredContacts, status } = useSelector(contactsSelector);
@@ -15,58 +26,62 @@ const ContactsList = () => {
 
   const contactsList = status === 'default' ? contacts : filteredContacts;
 
+  const feedback = getFeedback(status);
+
   return (
     <ul className="contacts-list">
-      {contactsList.slice().sort((a, b) => {
-        if (a.lastName < b.lastName) {
-          return -1;
-        }
-        if (a.lastName > b.lastName) {
-          return 1;
-        }
-        return 0;
-      }).map(({
-        firstName,
-        lastName,
-        phoneNumber,
-        id,
-      }, index) => (
-        <li key={id} className="contacts-list__item">
-          <p className="contacts-list__number">
-            {index + 1}{'.'}
-          </p>
-          <div className="contacts-list__text-wrapper">
-            <p className="contacts-list__text">
-              {lastName}{' '}{firstName}
+      {contactsList.length === 0
+        ? feedback
+        : contactsList.slice().sort((a, b) => {
+          if (a.lastName < b.lastName) {
+            return -1;
+          }
+          if (a.lastName > b.lastName) {
+            return 1;
+          }
+          return 0;
+        }).map(({
+          firstName,
+          lastName,
+          phoneNumber,
+          id,
+        }, index) => (
+          <li key={id} className="contacts-list__item">
+            <p className="contacts-list__number">
+              {index + 1}{'.'}
             </p>
-            <p className="contacts-list__text">
-              +7{' '}
-              {phoneNumber}
-            </p>
-          </div>
-          <div className="contacts-list__link-wrapper">
-            <a
-              href="/edit"
-              onClick={showModal('renaming', {
-                lastName,
-                firstName,
-                phoneNumber,
-                id,
-              })}
-              className="contacts-list__link contacts-list__link--edit"
-            >
-              Редактировать
-            </a>
-            <a
-              href="/remove"
-              onClick={showModal('removing', { lastName, firstName, id })}
-              className="contacts-list__link contacts-list__link--remove"
-            >
-              Удалить
-            </a>
-          </div>
-        </li>
-      ))}
+            <div className="contacts-list__text-wrapper">
+              <p className="contacts-list__text">
+                {lastName}{' '}{firstName}
+              </p>
+              <p className="contacts-list__text">
+                +7{' '}
+                {phoneNumber}
+              </p>
+            </div>
+            <div className="contacts-list__link-wrapper">
+              <a
+                href="/edit"
+                onClick={showModal('renaming', {
+                  lastName,
+                  firstName,
+                  phoneNumber,
+                  id,
+                })}
+                className="contacts-list__link contacts-list__link--edit"
+              >
+                Редактировать
+              </a>
+              <a
+                href="/remove"
+                onClick={showModal('removing', { lastName, firstName, id })}
+                className="contacts-list__link contacts-list__link--remove"
+              >
+                Удалить
+              </a>
+            </div>
+          </li>
+        ))}
     </ul>
   );
 };
